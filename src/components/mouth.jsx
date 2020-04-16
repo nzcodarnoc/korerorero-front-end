7e55ccdf573bcd13107dc78229bbde59c863b0b0
Conrad Johnston <conrad@darnoc.co.nz>
2020-04-17 10:51:47 +1200

    Wire up mouth animation to API response in component.

--- src/components/mouth.jsx
+++ src/components/mouth.jsx
@@ -1,47 +1,33 @@
 import React, { useEffect } from "react";
 import anime from "animejs";
 import getConfig from "next/config";
-import {Howl, Howler} from 'howler';
+import { Howl, Howler } from "howler";
 const { publicRuntimeConfig } = getConfig();
 const MOUTH_SHAPES = `${publicRuntimeConfig.staticFolder}/mouth-shapes`;
+import assembleTimeline from "./helpers/assemble-timeline";
 
-function Mouth({audio, shapes}) {
+function Mouth({ audio, shapes }) {
   useEffect(() => {
+    anime.set('#shape-A', {
+      opacity: "1"
+    });
+    if (!audio || !shapes) return;
     const sound = new Howl({
       src: [audio],
-      format: ['wav']
+      format: ["wav"],
     });
-    
-    sound.play();
     const timeline = anime.timeline({
       targets: ".mouth",
-      autoplay: true,
-      loop: false, // it can't loop because we are using absolute time offsets
-      complete: (x) => doRestart(),
+      autoplay: false,
+      loop: false,
     });
-
-    function doRestart() {
-      timeline.restart();
-    }
-    let absTime = 0;
-    ["A", "B", "C", "D", "E", "F", "G", "H", "X"].map((shape) => {
-      timeline.add(
-        {
-          targets: `#shape-${shape}`,
-          opacity: "1",
-        },
-        absTime
-      );
-      absTime += 500;
-      timeline.add(
-        {
-          targets: `#shape-${shape}`,
-          opacity: "0",
-        },
-        absTime
-      );
+    assembleTimeline(timeline, shapes);
+    anime.set('#shape-A', {
+      opacity: "0"
     });
-  }, []);
+    sound.play();
+    timeline.play();
+  });
   return (
     <div className="container">
       <main>
@@ -52,49 +38,48 @@ function Mouth({audio, shapes}) {
             const id = `shape-${shape}`;
             return (
               <div key={id} className="mouth" id={id}>
-                <img src={src} alt="" width="100%" /> {shape}
+                <img src={src} alt="" width="100%" />
               </div>
             );
           })}
         </div>
       </main>
-     <style jsx>{`
-       main {
-         padding: 5rem 0;
-         flex: 1;
-         display: flex;
-         flex-direction: column;
-         justify-content: center;
-         align-items: center;
-       }
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-       .title {
-         margin: 0;
-         line-height: 1.15;
-         font-size: 4rem;
-         text-align: center;
-       }
-       .mouth-container {
-         margin-left: -100vw;
-       }
-       .mouth {
-         position: absolute;
-         opacity: 0;
-       }
-     `}</style>
-     <style jsx global>{`
-       html,
-       body {
-         padding: 0;
-         margin: 0;
-         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-           Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-           sans-serif;
-       }
-     `}</style>
-
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+          text-align: center;
+        }
+        .mouth-container {
+          margin-left: -100vw;
+        }
+        .mouth {
+          position: absolute;
+          opacity: 0;
+        }
+      `}</style>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+      `}</style>
     </div>
   );
-};
+}
 
 export default Mouth;
