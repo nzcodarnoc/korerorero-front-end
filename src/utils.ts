71495822b2c3a0446c442fda3c1a8a7a482fd55b
Conrad Johnston <conrad@darnoc.co.nz>
2020-05-17 09:39:09 +1200

    Change the names in the validator so error messages are meaniful.

--- src/utils.ts
+++ src/utils.ts
@@ -2,25 +2,26 @@
 import envalid, { str, url } from "envalid";
 
 // See: https://nextjs.org/docs/basic-features/environment-variables
-// Trying to destructure process.env variables won't work due to the 
+// Trying to destructure process.env variables won't work due to the
 // limitations of webpack's DefinePlugin.
 const env = envalid.cleanEnv(
   {
-    SELF_PATH: process.env.NEXT_PUBLIC_SELF_PATH,
-    ORCHESTRATION_ENDPOINT: process.env.NEXT_PUBLIC_ORCHESTRATION_ENDPOINT,
-    RECOGNIZER_HOST: process.env.NEXT_PUBLIC_RECOGNIZER_HOST,
-    RECOGNIZER_PATH: process.env.NEXT_PUBLIC_RECOGNIZER_PATH,
+    NEXT_PUBLIC_SELF_PATH: process.env.NEXT_PUBLIC_SELF_PATH,
+    NEXT_PUBLIC_ORCHESTRATION_ENDPOINT:
+      process.env.NEXT_PUBLIC_ORCHESTRATION_ENDPOINT,
+    NEXT_PUBLIC_RECOGNIZER_HOST: process.env.NEXT_PUBLIC_RECOGNIZER_HOST,
+    NEXT_PUBLIC_RECOGNIZER_PATH: process.env.NEXT_PUBLIC_RECOGNIZER_PATH,
   },
   {
-    SELF_PATH: str(),
-    ORCHESTRATION_ENDPOINT: url(),
-    RECOGNIZER_HOST: url(),
-    RECOGNIZER_PATH: str(),
+    NEXT_PUBLIC_SELF_PATH: str(),
+    NEXT_PUBLIC_ORCHESTRATION_ENDPOINT: url(),
+    NEXT_PUBLIC_RECOGNIZER_HOST: url(),
+    NEXT_PUBLIC_RECOGNIZER_PATH: str(),
   }
 );
 
-export const ORCHESTRATION_ENDPOINT = env.ORCHESTRATION_ENDPOINT;
-export const RECOGNIZER_HOST = env.RECOGNIZER_HOST;
-export const RECOGNIZER_PATH = env.RECOGNIZER_PATH;
-export const STATIC_PATH = env.SELF_PATH;
+export const ORCHESTRATION_ENDPOINT = env.NEXT_PUBLIC_ORCHESTRATION_ENDPOINT;
+export const RECOGNIZER_HOST = env.NEXT_PUBLIC_RECOGNIZER_HOST;
+export const RECOGNIZER_PATH = env.NEXT_PUBLIC_RECOGNIZER_PATH;
+export const STATIC_PATH = env.NEXT_PUBLIC_SELF_PATH;
 export const MOUTH_SHAPES_PATH = STATIC_PATH + "mouth-shapes";
