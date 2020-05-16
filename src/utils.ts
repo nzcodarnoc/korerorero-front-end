09fb8fc0f0ca09be4c3560404d7e7cb3e7cf6f25
Conrad Johnston <conrad@darnoc.co.nz>
2020-05-17 09:44:18 +1200

    Remove envalid because its validation strategy seems to conflict with next.js handling of env variables.

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,5 @@
-// see also next.config.js where process.env is set using webpack
-import envalid, { str, url } from "envalid";
-
-// See: https://nextjs.org/docs/basic-features/environment-variables
-// Trying to destructure process.env variables won't work due to the
-// limitations of webpack's DefinePlugin.
-const env = envalid.cleanEnv(
-  {
-    NEXT_PUBLIC_SELF_PATH: process.env.NEXT_PUBLIC_SELF_PATH,
-    NEXT_PUBLIC_ORCHESTRATION_ENDPOINT:
-      process.env.NEXT_PUBLIC_ORCHESTRATION_ENDPOINT,
-    NEXT_PUBLIC_RECOGNIZER_HOST: process.env.NEXT_PUBLIC_RECOGNIZER_HOST,
-    NEXT_PUBLIC_RECOGNIZER_PATH: process.env.NEXT_PUBLIC_RECOGNIZER_PATH,
-  },
-  {
-    NEXT_PUBLIC_SELF_PATH: str(),
-    NEXT_PUBLIC_ORCHESTRATION_ENDPOINT: url(),
-    NEXT_PUBLIC_RECOGNIZER_HOST: url(),
-    NEXT_PUBLIC_RECOGNIZER_PATH: str(),
-  }
-);
-
-export const ORCHESTRATION_ENDPOINT = env.NEXT_PUBLIC_ORCHESTRATION_ENDPOINT;
-export const RECOGNIZER_HOST = env.NEXT_PUBLIC_RECOGNIZER_HOST;
-export const RECOGNIZER_PATH = env.NEXT_PUBLIC_RECOGNIZER_PATH;
-export const STATIC_PATH = env.NEXT_PUBLIC_SELF_PATH;
+export const ORCHESTRATION_ENDPOINT = process.env.NEXT_PUBLIC_ORCHESTRATION_ENDPOINT;
+export const RECOGNIZER_HOST = process.env.NEXT_PUBLIC_RECOGNIZER_HOST;
+export const RECOGNIZER_PATH = process.env.NEXT_PUBLIC_RECOGNIZER_PATH;
+export const STATIC_PATH = process.env.NEXT_PUBLIC_SELF_PATH;
 export const MOUTH_SHAPES_PATH = STATIC_PATH + "mouth-shapes";
